bb62615db88712b6ec6dad0b18aea26e88c1eca9
Gustavo Keller <gustavo.lealtec@gmail.com>
2023-02-12 10:47:32 -0300

    Update table clientes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,20 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../../src/styles/Home.module.css'
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('João', 23, '2'),
+    new Cliente('Maria', 16, '3'),
+    new Cliente('Pedro', 29, '4'),
+  ]
+
   return (
         <div className={`
           flex h-screen justify-center items-center
@@ -14,7 +24,7 @@ export default function Home() {
           text-white
         `}>
           <Layout titulo='Cadastro Simples'>
-            <span>Conteudo</span>
+            <Tabela clientes={clientes}></Tabela>
           </Layout>
         </div>
   )
